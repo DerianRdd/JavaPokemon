f68e2417a65d6c1affbb87a1aa06c34faca351c1
Wildfire3212 <72580919+Wildfire3212@users.noreply.github.com>
2020-12-09 00:28:34 -0600

    Pokemon (Solo variables, faltan metodos)

---
+++ Pokemon.java
@@ -0,0 +1,18 @@
+public class Pokemon {
+    private String Apodo;
+    //Puntos de vida maximos para sacar porcientos
+    private int vidaMax;
+    //Puntos de vida actuales
+    private int vida;
+    //Ataque
+    private int ataque;
+    //Defensa
+    private int defensa;
+    //Velocidad
+    private int velocidad;
+    //Tipo
+    private String tipo;
+    //Estado
+    private String estado;
+    
+}
